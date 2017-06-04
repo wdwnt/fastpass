8f6dcba48c3c37ba91bf0662c7b3d39148810411
Michael V. Battista <github@mvbattista.com>
2017-06-04 17:42:04 -0400

    Set up cache server with posts

---
+++ fastpass.py
@@ -0,0 +1,33 @@
+import requests
+import requests_cache
+import redis
+from flask import Flask, jsonify
+# import time
+
+POSTS_PER_PAGE = 30
+REDIS_HOST = '127.0.0.1'
+REDIS_PORT = 36379
+REDIS_PASSWORD = ''
+CACHE_EXPIRE_SECONDS = 180
+
+app = Flask(__name__)
+redis_conn = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD)
+
+requests_cache.install_cache('fastpass_cache', backend='redis',
+                             expire_after=CACHE_EXPIRE_SECONDS, connection=redis_conn)
+
+
+@app.route('/posts/<int:page>')
+@app.route('/posts', defaults={'page': 1})
+def posts(page):
+    url = 'http://wdwnt.com/wp-json/wp/v2/posts?_embed&per_page={}&page={}'.format(POSTS_PER_PAGE, page)
+    response = requests.get(url)
+    response_dict = response.json()
+    # Uncomment for debugging
+    # now = time.ctime(int(time.time()))
+    # print("Time: {0} / Used Cache: {1}".format(now, response.from_cache))
+    return jsonify(response_dict)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
