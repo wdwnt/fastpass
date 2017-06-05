6f0387d35b70c50dc44a8b17d4c464761221b8ed
Michael V. Battista <github@mvbattista.com>
2017-06-04 20:40:17 -0400

    Clean up for deployment

--- fastpass.py
+++ fastpass.py
@@ -1,14 +1,17 @@
+# import time
+import os
+
 import requests
 import requests_cache
 import redis
 from flask import Flask, jsonify
-# import time
 
-POSTS_PER_PAGE = 30
-REDIS_HOST = '127.0.0.1'
-REDIS_PORT = 36379
-REDIS_PASSWORD = ''
-CACHE_EXPIRE_SECONDS = 180
+SERVER_PORT = os.getenv('FASTPASS_HOST_PORT', 80)
+POSTS_PER_PAGE = os.getenv('FASTPASS_POSTS_PER_PAGE', 30)
+REDIS_HOST = os.getenv('FASTPASS_REDIS_HOST', '127.0.0.1')
+REDIS_PORT = os.getenv('FASTPASS_REDIS_PORT', 36379)
+REDIS_PASSWORD = os.getenv('FASTPASS_REDIS_PASSWORD', '')
+CACHE_EXPIRE_SECONDS = os.getenv('FASTPASS_CACHE_EXPIRE_SECONDS', 180)
 
 app = Flask(__name__)
 redis_conn = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD)
@@ -30,4 +33,4 @@ def posts(page):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=SERVER_PORT)
