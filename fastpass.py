5acaa222c69d2249e7df2bc4f4459ac4e38be6e4
Michael V. Battista <github@mvbattista.com>
2018-05-30 23:13:13 -0400

    Refactor to use memory

--- fastpass.py
+++ fastpass.py
@@ -1,6 +1,7 @@
 # import time
 import os
-
+from datetime import datetime, timedelta
+import time
 import requests
 import requests_cache
 import redis
@@ -14,21 +15,45 @@ REDIS_PASSWORD = os.getenv('FASTPASS_REDIS_PASSWORD', '')
 CACHE_EXPIRE_SECONDS = os.getenv('FASTPASS_CACHE_EXPIRE_SECONDS', 180)
 
 app = Flask(__name__)
-redis_conn = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD)
+# redis_conn = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD)
+#
+# requests_cache.install_cache('fastpass_cache', backend='redis',
+#                              expire_after=CACHE_EXPIRE_SECONDS, connection=redis_conn)
+mem_cache = {}
 
-requests_cache.install_cache('fastpass_cache', backend='redis',
-                             expire_after=CACHE_EXPIRE_SECONDS, connection=redis_conn)
+def _store_in_cache(url, data, expire_time=None, expire_seconds=CACHE_EXPIRE_SECONDS):
+    if not expire_time:
+        expire_time = datetime.now() + timedelta(seconds=expire_seconds)
+    val = {
+        'data': data,
+        'expire_at': expire_time.timestamp(),
+    }
+    mem_cache[url] = val
+
+def _get_from_cache(url):
+    if url not in mem_cache:
+        return None
+    if mem_cache[url]['expire_at'] >= datetime.now().timestamp():
+        return mem_cache[url]['data']
+    else:
+        del mem_cache[url]
+        return None
 
 
 @app.route('/posts/<int:page>')
 @app.route('/posts', defaults={'page': 1})
 def posts(page):
-    url = 'http://wdwnt.com/wp-json/wp/v2/posts?_embed&per_page={}&page={}'.format(POSTS_PER_PAGE, page)
-    response = requests.get(url)
-    response_dict = response.json()
-    # Uncomment for debugging
-    # now = time.ctime(int(time.time()))
-    # print("Time: {0} / Used Cache: {1}".format(now, response.from_cache))
+    url = 'http://wdwnt.com/wp-json/wp/v2/posts?per_page={}&page={}'.format(POSTS_PER_PAGE, page)
+    print(url)
+    response_dict =  _get_from_cache(url)
+    if not response_dict:
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/50.0.2661.102 Safari/537.36'}
+        response = requests.get(url, headers=headers)
+        response_dict = response.json()
+        _store_in_cache(url, response_dict)
+
     return jsonify(response_dict)
 
 
