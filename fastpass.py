48dea86f4d09e63074b72e06b879b4e4900a1b07
Michael V. Battista <github@mvbattista.com>
2018-06-01 11:07:35 -0400

    Cleaned up & slimmed down requirements and Airtime call

--- fastpass.py
+++ fastpass.py
@@ -1,11 +1,9 @@
 # import time
-from pprint import pprint
+
 import os
 from datetime import datetime, timedelta, timezone
-import time
+
 import requests
-import requests_cache
-import redis
 from flask import Flask, jsonify
 
 SERVER_PORT = os.getenv('FASTPASS_HOST_PORT', 80)
@@ -16,14 +14,29 @@ REDIS_PASSWORD = os.getenv('FASTPASS_REDIS_PASSWORD', '')
 CACHE_EXPIRE_SECONDS = os.getenv('FASTPASS_CACHE_EXPIRE_SECONDS', 180)
 
 app = Flask(__name__)
-# redis_conn = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD)
-#
-# requests_cache.install_cache('fastpass_cache', backend='redis',
-#                              expire_after=CACHE_EXPIRE_SECONDS, connection=redis_conn)
+
 mem_cache = {}
 
 
-def _store_in_cache(url, data, expire_time=None, expire_seconds=CACHE_EXPIRE_SECONDS):
+def format_airtime(in_data):
+    result = {'current': {}, 'currentShow': []}
+    result['current']['ends'] = in_data.get('current', {}).get('ends')
+    for field in ('track_title', 'artist_title', 'length'):
+        result['current'][field] = in_data.get('current', {})\
+            .get('metadata', {})\
+            .get(field)
+    if len(in_data.get('currentShow', [])):
+        show_data = in_data['currentShow'][0]
+        show_field = {}
+        for field in ('name', 'image_path'):
+            show_field[field] = show_data.get(field)
+        result['currentShow'].append(show_field)
+
+    return result
+
+
+def _store_in_cache(url, data, expire_time=None,
+                    expire_seconds=CACHE_EXPIRE_SECONDS):
     if not expire_time:
         expiry = datetime.utcnow() + timedelta(seconds=expire_seconds)
         expiry = expiry.replace(tzinfo=timezone.utc)
@@ -32,24 +45,19 @@ def _store_in_cache(url, data, expire_time=None, expire_seconds=CACHE_EXPIRE_SEC
 
     val = {
         'data': data,
-        'expire_at': expiry,
+        'expire_at': expiry.timestamp(),
     }
-    print('Now from _store_in_cache:')
-    pprint(expiry)
     mem_cache[url] = val
+
 
 def _get_from_cache(url):
     now = datetime.utcnow()
     now = now.replace(tzinfo=timezone.utc)
-    print('Now from _get_from_cache:')
-    pprint(now)
     if url not in mem_cache:
         return None
-    if mem_cache[url]['expire_at'] >= now:
-        print('e {} - c {}'.format(mem_cache[url]['expire_at'], now))
+    if mem_cache[url]['expire_at'] >= now.timestamp():
         return mem_cache[url]['data']
     else:
-        print('DEL e {} - c {}'.format(mem_cache[url]['expire_at'], now))
         del mem_cache[url]
         return None
 
@@ -57,12 +65,14 @@ def _get_from_cache(url):
 @app.route('/posts/<int:page>')
 @app.route('/posts', defaults={'page': 1})
 def posts(page):
-    url = 'http://wdwnt.com/wp-json/wp/v2/posts?per_page={}&page={}'.format(POSTS_PER_PAGE, page)
+    url = 'http://wdwnt.com/wp-json/wp/v2/posts?per_page={}&page={}'
+    url = url.format(POSTS_PER_PAGE, page)
     # print(url)
-    response_dict =  _get_from_cache(url)
+    response_dict = _get_from_cache(url)
     if not response_dict:
         headers = {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) '
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) '
+                          'AppleWebKit/537.36 (KHTML, like Gecko) '
                           'Chrome/50.0.2661.102 Safari/537.36'}
         response = requests.get(url, headers=headers)
         response_dict = response.json()
@@ -73,12 +83,12 @@ def posts(page):
 @app.route('/radio')
 def radio():
     url = 'https://wdwnt.airtime.pro/api/live-info'
+    date_form = '%Y-%m-%d %H:%M:%S'
     response_dict = _get_from_cache(url)
     if not response_dict:
         response = requests.get(url)
-        response_dict = response.json()
-        print('Fresh call')
-        ending = datetime.strptime(response_dict['current']['ends'], '%Y-%m-%d %H:%M:%S')
+        response_dict = format_airtime(response.json())
+        ending = datetime.strptime(response_dict['current']['ends'], date_form)
         ending = ending.replace(tzinfo=timezone.utc)
         # pprint(ending)
         _store_in_cache(url, response_dict, expire_time=ending)
